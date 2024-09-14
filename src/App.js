52affd0d33b8c3e827d4141146cba39e8b72ef60
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-09-14 15:26:53 -0500

    add background color and event management route

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
-import Navbar from './Components/Navbar'; // Import the Navbar
+import EventManagementForm from './Pages/EventManagementForm';
+import Navbar from './Components/Navbar';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background: '#E2DAD6',
+          minHeight: '100vh',
+          margin: 0,
+          padding: 0,
+          boxSizing: 'border-box',
+        },
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar /> {/* Add the Navbar */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/event-management" element={<EventManagementForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
